4066e7fa9c471a30fd88441196f2d5c2902ab90d
mr-abhi0 <durgapr0786@gmail.com>
2023-08-29 16:25:25 +0530

    orchid removed

--- app/Http/Livewire/DetailsComponent.php
+++ app/Http/Livewire/DetailsComponent.php
@@ -8,13 +8,16 @@ use Livewire\Component;
 class DetailsComponent extends Component
 {
     public $slug;
-    public function mount($slug){
-        $this->slug = $slug
-
+    public function mount($slug)
+    {
+        $this->slug = $slug;
     }
     public function render()
     {
-        $product = Product::where('slug',$this->slug)->first();
-        return view('livewire.details-component',['product'=>$product])->layout('layouts.base');
+        $product = Product::where('slug', $this->slug)->first();
+        $popular_products = Product::inRandomOrder()->limit(4)->get();
+        $related_products = Product::where('category_id', $product->category_id)->inRandomOrder()->limit(5)->get();
+
+        return view('livewire.details-component', ['product' => $product, 'popular_products' => $popular_products, 'related_products' => $related_products])->layout('layouts.base');
     }
 }
